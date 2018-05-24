2d2c9aefd18c2496b3a63017448e75134eaf05de
Robert Tabares <robtabares92@gmail.com>
2018-05-24 19:48:02 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,19 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |suite, name|
+    if suite == :suite_a and name[0] == "A"
+      return name
+    end
+  end
 end
+
+# def select_winner(passengers)
+#   # add the code snippet here!
+# end
